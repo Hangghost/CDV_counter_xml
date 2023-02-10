425f1dc7cdadf3f52e669ae74e6e1a3b7d4474cf
Hung Lun Chen <hangghost@gmail.com>
2023-02-10 13:45:58 +0800

    Finished v1 commit

--- Tools.py
+++ Tools.py
@@ -1,3 +1,15 @@
 def print_type(object):
     print(object)
     print(type(object))
+
+
+def CDV_counter(CDV_name, tag_list, other_name = None):
+    CDV_count = 0
+    for element in tag_list:
+        CDV_count += element.count(str(CDV_name))
+        if CDV_count == 0:
+            CDV_count += element.count(str(other_name))
+    return CDV_count
+
+
+        
--- main.py
+++ main.py
@@ -19,58 +19,65 @@ for filename in xml_filename_list:
     with open(filename, "r", encoding="UTF-16") as file:   # iterate all files
         data = file.read()
     bs_data = BeautifulSoup(data, "xml")
+
     Leftstatement = bs_data.find_all("Leftstatement")   # Put all the tag in the list 
     Leftstatement_list = []
     for element in Leftstatement:
         Leftstatement_list.append(str(element))
-    Study_id = str(bs_data.StudyUID.string)
-    Severity = str(bs_data.Severity.string)
-    Leftstatement_dic[Study_id] = [Leftstatement_list, Severity]    # Creat a dictionary
-    
-Tools.print_type(Leftstatement_dic)
-
-print("--------------------------------")
-
-
-
-for ids, report in Leftstatement_dic.items():   # report: [Leftstatement list, severity]
-    leftstatement_list = report[0]
-    ST_count = 0
-    VPC_count = 0
-    APC_count = 0
-    JPC_count = 0
-    VF_count = 0
-    LBBB_count = 0
-    RBBB_count = 0
-    firstAVB_count = 0
-    secondtype1AVB_count = 0
-    secondtype2AVB_count = 0
-    LVH_count = 0
-    RVH_count = 0
-    STelevation_count = 0
-    STdepression_count = 0
-    LongQT_count = 0
-    SB_count = 0
-    for element in leftstatement_list:
-        ST_count += element.count("SINUS TACHYCARDIA")
-        VPC_count += element.count("VENTRICULAR PREMATURE COMPLEX")
-        APC_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        JPC_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        VF_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        LBBB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        RBBB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        firstAVB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        secondtype1AVB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        secondtype2AVB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        LVH_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        RVH_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        STelevation_count += element.count("ST ELEVATION")
-        STdepression_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        LongQT_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-        SB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
-    print(f"The ID is {ids}. The status is {Severity}. The VPC number is {VPC_count}. The ST number is {ST_count}")
-    # write in the CSV file
         
+    Study_id = str(bs_data.StudyUID.string)    # Get ID
+    Severity = str(bs_data.Severity.string)    # Get severity
+    date = str(bs_data.StudyDate.string)    # Get date
+
+    Leftstatement_dic[Study_id] = [Leftstatement_list, Severity, date]    # Creat a dictionary
 
 
-# 輸出成檔案
+CDV_name = ["Sinus tachycardia", "Ventriclar premature complex", "Atrial premature complex", "J premature complex", "Ventricular fibrillation", "Atrial fibrillation", "Left bundle branch block", "Right bundle branch block", "1st atrioventricular block", "2nd trioventricular block type 1", "2nd trioventricular block type 2", "Left ventricular hypertrophy", "Right ventricular hypertrophy", "ST elevation", "ST depression", "Long QT interval", "Sinus bradycardia", "Sinus arrhythmia"]
+CDV_counting_dic = {}
+
+
+for ids, report in Leftstatement_dic.items():   # report: [Leftstatement list, severity, date]
+    leftstatement_list = report[0]    # Get the Leftstatement list
+    Severity = report[1]
+    date = report[2]
+
+    ST_count = Tools.CDV_counter("SINUS TACHYCARDIA", leftstatement_list)
+    VPC_count = Tools.CDV_counter("VENTRICULAR PREMATURE", leftstatement_list, other_name="VENT PREMATURE COMPLEXES")
+    APC_count = Tools.CDV_counter("ATRIAL PREMATURE COMPLEX", leftstatement_list, other_name="MULTIPLE ATRIAL PREMATURE COMPLEXES")
+    JPC_count = Tools.CDV_counter("-", leftstatement_list)
+    VF_count = Tools.CDV_counter("-", leftstatement_list)
+    AF_count = Tools.CDV_counter("ATRIAL FIBRILLATION", leftstatement_list, other_name="ATRIAL FLUTTER/FIBRILLATION")
+    LBBB_count = Tools.CDV_counter("LEFT BUNDLE BRANCH BLOCK", leftstatement_list)
+    RBBB_count = Tools.CDV_counter("RIGHT BUNDLE BRANCH BLOCK", leftstatement_list, other_name="RBBB AND LPFB")
+    firstAVB_count = Tools.CDV_counter("FIRST DEGREE AV BLOCK", leftstatement_list)
+    secondtype1AVB_count = Tools.CDV_counter("-", leftstatement_list)
+    secondtype2AVB_count = Tools.CDV_counter("-", leftstatement_list)
+    LVH_count = Tools.CDV_counter("LEFT VENTRICULAR HYPERTROPHY", leftstatement_list, other_name="LVH WITH")
+    RVH_count = Tools.CDV_counter("RIGHT VENTRICULAR HYPERTROPHY", leftstatement_list)
+    STelevation_count = Tools.CDV_counter("ST ELEVATION", leftstatement_list, other_name="ST ELEV") 
+    STdepression_count = Tools.CDV_counter("ST DEPRESSION", leftstatement_list)
+    LongQT_count = Tools.CDV_counter("PROLONGED QT INTERVAL", leftstatement_list)
+    SB_count = Tools.CDV_counter("SINUS BRADYCARDIA", leftstatement_list)
+    SA_count = Tools.CDV_counter("SINUS ARRHYTHMIA", leftstatement_list)
+
+    CDV_counting_value = [ST_count, VPC_count, APC_count, JPC_count, VF_count, AF_count, LBBB_count, RBBB_count, firstAVB_count, secondtype1AVB_count, secondtype2AVB_count, LVH_count, RVH_count, STelevation_count, STelevation_count, STdepression_count, LongQT_count, SB_count, SA_count]
+
+    CDV_counting_nested_dic ={}
+    for i in range(len(CDV_name)):
+        CDV_counting_nested_dic[CDV_name[i]] = CDV_counting_value[i]
+   
+    CDV_counting_dic[ids] = [date, Severity, CDV_counting_nested_dic]
+
+new_file_name = "CDV counting data.csv"
+with open(new_file_name, "w") as file_object:
+    file_object.write("ID" +","+ "Date" +","+ "Severity")    # Write the title
+    for name in CDV_name:
+        file_object.write(","+ name)
+    file_object.write("\n")
+
+    for ID, content in CDV_counting_dic.items():
+        file_object.write(ID +","+ content[0] +","+ content[1])
+        nested_dic = CDV_counting_dic[ID][2]
+        for CDV_values in nested_dic.values():
+            file_object.write(","+ str(CDV_values))
+        file_object.write("\n")
