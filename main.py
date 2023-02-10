8ddb427852c1140cef48e16cdc3503b0d3a8e024
Hung Lun Chen <hangghost@gmail.com>
2023-02-10 10:19:29 +0800

    commit

--- main.py
+++ main.py
@@ -24,16 +24,52 @@ for filename in xml_filename_list:
     for element in Leftstatement:
         Leftstatement_list.append(str(element))
     Study_id = str(bs_data.StudyUID.string)
-    Leftstatement_dic[Study_id] = Leftstatement_list    # Creat a dictionary
+    Severity = str(bs_data.Severity.string)
+    Leftstatement_dic[Study_id] = [Leftstatement_list, Severity]    # Creat a dictionary
     
 Tools.print_type(Leftstatement_dic)
 
 print("--------------------------------")
 
-for id, leftstatement_list in Leftstatement_dic.items():
+
+
+for ids, report in Leftstatement_dic.items():   # report: [Leftstatement list, severity]
+    leftstatement_list = report[0]
+    ST_count = 0
+    VPC_count = 0
+    APC_count = 0
+    JPC_count = 0
+    VF_count = 0
+    LBBB_count = 0
+    RBBB_count = 0
+    firstAVB_count = 0
+    secondtype1AVB_count = 0
+    secondtype2AVB_count = 0
+    LVH_count = 0
+    RVH_count = 0
+    STelevation_count = 0
+    STdepression_count = 0
+    LongQT_count = 0
+    SB_count = 0
     for element in leftstatement_list:
-        count = element.count("VENTRICULAR PREMATURE COMPLEXES")
-        print(f"The ID is {id}. The count is {count}")
+        ST_count += element.count("SINUS TACHYCARDIA")
+        VPC_count += element.count("VENTRICULAR PREMATURE COMPLEX")
+        APC_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        JPC_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        VF_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        LBBB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        RBBB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        firstAVB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        secondtype1AVB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        secondtype2AVB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        LVH_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        RVH_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        STelevation_count += element.count("ST ELEVATION")
+        STdepression_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        LongQT_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+        SB_count += element.count("VENTRICULAR PREMATURE COMPLEXES")
+    print(f"The ID is {ids}. The status is {Severity}. The VPC number is {VPC_count}. The ST number is {ST_count}")
+    # write in the CSV file
         
 
 
